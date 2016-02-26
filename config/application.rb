01eb9aae617eae8b1f4ca50e565152e676ad14fe
juliemiller <jemiller148@gmail.com>
2016-02-25 16:35:30 -0800

    fix heroku error

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+config.assets.initialize_on_precompile = false
+
 Bundler.require(*Rails.groups)
 
 module SampleProjectProposalMaster

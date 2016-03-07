99d7c6fcdbe70efa2c6108db3c72d8d9c09b78b3
juliemiller <jemiller148@gmail.com>
2016-03-06 18:46:31 -0800

    update project title

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/finalproject.jsx",
+  entry: "./frontend/cuttingboard.jsx",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js",
